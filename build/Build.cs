0af1bbc4a5091489bbb218ae76e32af99ecaf334
José Manuel Nieto (SuperJMN) <superjmn@outlook.com>
2024-11-22 23:36:08 +0100

    Add nuke files

---
+++ build/Build.cs
@@ -0,0 +1,43 @@
+using CSharpFunctionalExtensions;
+using Nuke.Common;
+using Nuke.Common.Git;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using Zafiro.Deployment;
+using Zafiro.Misc;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+class Build : NukeBuild
+{
+    [Parameter("GitHub Authentication Token")] [Secret] readonly string GitHubApiKey;
+    [Parameter] readonly bool Force;
+    [Solution] readonly Solution Solution;
+    [GitVersion] readonly GitVersion GitVersion;
+    [GitRepository] readonly GitRepository Repository;
+
+    public static int Main() => Execute<Build>(x => x.Publish);
+
+    Target RestoreWorkloads => td => td
+        .Executes(() =>
+        {
+            DotNetWorkloadRestore(x => x.SetProject(Solution));
+        });
+
+    Target PublishSite => d => d
+        .Requires(() => GitHubApiKey)
+        .DependsOn(RestoreWorkloads)
+        .OnlyWhenStatic(() => Repository.IsOnMainOrMasterBranch() || Force)
+        .Executes(async () =>
+        {
+            await Solution.AllProjects.TryFirst(project => project.Name.EndsWith(".Browser"))
+                .ToResult("Browser project not found")
+                .Map(project => project.Path.ToString())
+                .Bind(projectPath => Deployer.Instance.PublishAvaloniaAppToGitHubPages(projectPath, "SuperJMN", "LongevityWorldCup.Site", GitHubApiKey))
+                .TapError(error => Assert.Fail(error.ToString()))
+                .LogInfo("Site published");
+        });
+
+    Target Publish => td => td
+        .DependsOn(PublishSite);
+}
