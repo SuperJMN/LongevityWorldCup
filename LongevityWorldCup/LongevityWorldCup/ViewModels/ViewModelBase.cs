753bd47e606c34f51c7bd448901c52bfb80fed95
José Manuel Nieto (SuperJMN) <superjmn@outlook.com>
2024-11-22 17:20:10 +0100

    Initial files

---
+++ LongevityWorldCup/LongevityWorldCup/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace LongevityWorldCup.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ LongevityWorldCup/LongevityWorldCup/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace LongevityWorldCup.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
